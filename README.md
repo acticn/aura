b955a4e07694f618ab39d9c4a36f1c79de7549c4
acticn <acticns@gmail.com>
2022-11-25 16:18:17 +0800

    shader

--- README.md
+++ README.md
@@ -24,3 +24,16 @@ void processInput(GLFWwindow* window);
 设置uniform值
 
 
+
+
+# C++输入输出  
+ ![图片](./DocumenHelp/images/C%2B%2Bio.jpg)  
+ C++库中流类的继承关系  
+ 
+ 图 1 中这些流类各自的功能分别为：
++ istream：常用于接收从键盘输入的数据；  
++ ostream：常用于将数据输出到屏幕上；  
++ ifstream：用于读取文件中的数据；  
++ ofstream：用于向文件中写入数据；  
++ iostream：继承自 istream 和 ostream 类，因为该类的功能兼两者于一身，既能用于输入，也能用于输出；  
++ fstream：兼 ifstream 和 ofstream 类功能于一身，既能读取文件中的数据，又能向文件中写入数据。  
--- aura.cpp
+++ aura.cpp
@@ -21,7 +21,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//指定主版本号
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//指定次版本号
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//指定CORE模式
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //On osx
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     //下面创建一个窗口对象
     /*
@@ -37,6 +39,7 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);//绑定窗口
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
     //下面配置glad
@@ -46,26 +49,62 @@ int main()
         return -1;
     }
 
+
+
+
+    Shader ourShader("./src/shaders/3.3.shader.vs", "./src/shaders/3.3.shader.fs");//创建Shader
+    //顶点
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    };
+    //配置VBO
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    //解释顶点坐标
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    //解释顶点颜色
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     /*
     * glViewPort(GLint x, GLint y, GLsizei width, GLsizei height)
     * 前两个参数控制左下角的位置
     * 第三个和第四个参数控制渲染窗口的宽度和高度
     */
-    glViewport(0, 0, 800, 600);
+    ///glViewport(0, 0, 800, 600);
 
     //设置窗口调整回调函数，从而在窗口大小改变时调整渲染窗口大小
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+  
     //render loop
     while (!glfwWindowShouldClose(window))//glfwWindowShouldClose用于判断是否退出
     {
         processInput(window);
+        
+        glClearColor(.2f, .2f, .2f, .2f);//设置清空颜色
+        glClear(GL_COLOR_BUFFER_BIT);//执行清空命令
 
-        glClearColor(.2f, .2f, .2f, .2f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        ourShader.use();//启用program
+        glBindVertexArray(VAO);//绑定VAO 每次program进行渲染都需要指定一个VAO
+        glDrawArrays(GL_TRIANGLES, 0, 3);//以draw array方式进行渲染
+
+
         glfwSwapBuffers(window);//交换前后缓冲区（这种机制可以避免画面撕裂等现象
         glfwPollEvents();//检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用回调函数（可以通过回调方法手动设置）。
     }
+
+    //释放资源
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     glfwTerminate();//中止，释放资源
 
     return 0;
